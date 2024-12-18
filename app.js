e3972699a6feae8f12047b327aabc68759108bcb
ssemamborodney <163673782+ssemamb@users.noreply.github.com>
2024-12-18 10:51:50 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+function showsidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+
+function hidesidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+console.log("good");
+let gender = "apple";
+switch (gender) {
+    case "male":
+        console.log("your right");
+        break;
+    case "female":
+        console.log("your not a perfect match");
+        break;
+    default:
+        console.error("try again later");
+}
+console.log('good');
+
+function1();
+
+function function1() {
+    let x = 1;
+    console.log(x);
+}
+
+function function2() {
+    let x = 2;
+    console.log(x);
+}
